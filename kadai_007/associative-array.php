47c3f897cbc07f4e8ef697796e521484f3163339
shinomiya15-05 <ssssatoruuuu@gmail.com>
2025-12-26 20:28:40 +0900

    kadai_007提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset = 'UTF-8'>
+    <title>kadai_007</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      $user_data = ['name' => 'onion' , 'price' => 200 , 'weight' => 160];
+      echo '<br>';
+      print_r($user_data);
+
+      ?>
+    </p>
+  </body>
+</html>
